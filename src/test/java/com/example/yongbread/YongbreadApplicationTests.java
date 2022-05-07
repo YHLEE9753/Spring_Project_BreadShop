a121619d1e8099a0401e3614c633dff10546315c
dldydgns530@gmail.com <dldydgns530@gmail.com>
2022-05-07 19:49:49 +0900

    feat: Rest API 설정 및 DTO 작성

---
+++ src/test/java/com/example/yongbread/YongbreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yongbread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YongbreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
